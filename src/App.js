e5ed9f4cd415b780d6198f236e1be33a70697718
Martin Eley <meley@salesforce.com>
2022-08-26 16:57:57 +0100

    Added nice UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { Container, Row, Col } from 'react-bootstrap';
 import './App.css';
 import LoanCalculator from './components/loanCalculator';
+import herokuLogo from './heroku-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <LoanCalculator></LoanCalculator>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col xs={1} sm={2} md={3} lg={4}></Col>
+        <Col>
+          <LoanCalculator></LoanCalculator>
+        </Col>
+        <Col xs={1} sm={2} md={3} lg={4}></Col>
+      </Row>
+      <Row>
+        <Col xs={1} sm={2} md={3} lg={4}></Col>
+        <Col>
+          <p className="text-center">Powered by <img src={herokuLogo} height="40"/></p>
+        </Col>
+        <Col xs={1} sm={2} md={3} lg={4}></Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/components/loanCalculator.js
+++ src/components/loanCalculator.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useMemo } from "react";
 import axios from 'axios';
 import spinner from '../spinner.gif';
+import { Button, Container, Form, InputGroup, Card, Row, Col, ListGroup } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './loanCalculator.css';
 
 class LoanCalculator extends React.Component {
     constructor(props) {
@@ -24,60 +27,70 @@ class LoanCalculator extends React.Component {
 
     getRate() {
         this.setState({fetchingRate: true});
-        axios.get('https://heroku-api-demo-staging.herokuapp.com/api/v1/rate', { params: {
-            amount: this.state.amount, 
-            months: this.state.months
-        }}).then(res => {
-            this.setState({
-                fetchingRate: false,
-                rate: res.data.rate__c,
-                fresh: false
-            });
+            axios.get('https://heroku-api-demo-staging.herokuapp.com/api/v1/rate', { params: {
+                amount: this.state.amount, 
+                months: this.state.months
+            }}).then(res => {
+                setTimeout(() => { // Pause for dramatic effect
+                    this.setState({
+                        fetchingRate: false,
+                        rate: res.data.rate__c,
+                        fresh: false
+                    });
+                }, 1700);
         });
+        
+    }
+
+    emi = (p, rt, n) => {
+        const r = rt/100/n;
+        var x = Math.pow(1 + r, n);
+        var monthly = (p*x*r)/(x-1);
+        return monthly;
     }
 
     render() {
 
-        let rateSpan;
+        let rate;
         if(this.state.rate) {
-            rateSpan = <span>Your rate is {this.state.rate}</span>
+            rate = <div>
+                <p>Principle loan amount {this.state.amount}</p>
+                <p>Your rate is {this.state.rate}%</p>
+                <p>{this.state.months} monthly installments of £{this.emi(this.state.amount,this.state.rate,this.state.months)}</p>
+                <p>Total amount payable over {this.state.months} months is £{this.emi(this.state.amount,this.state.rate,this.state.months) * this.state.months}</p>
+                <Button>Apply NOW</Button>
+              </div>
         } else if(this.state.fresh) {
-            rateSpan = <span>Click Get my rate</span>
+            rate = <p>Click <i>Get my quote</i></p>
         } else if(!this.state.fresh) {
-            rateSpan = <span>No rate available</span>
+            rate = <p>No rate available</p>
         }
 
         let loading;
         if(this.state.fetchingRate) {
-            loading = <img src={spinner} alt="Waiting for rate"/>
+            loading = <p><img src={spinner} alt="Waiting for rate"/></p>
         }
 
         return(
-            <div>
-                <h1>Loan Calculator</h1>
-                <p>
-                    <label for="amount">Loan Amount</label>
-                    <br/>
-                    <input id="amount" name="amount" type="number" value={this.state.amount} onChange={(e) => this.handleChange('amount', e)}></input>
-                </p>
-                <p>
-                    <label for="months">Repayment Terms</label>
-                    <br/>
-                    <select id="months" name="months" value={this.state.months} onChange={(e) => this.handleChange('months', e)}>
-                    <option selected disabled>-- Repayment --</option>
-                    <option value="12">12</option>
-                    <option value="24">24</option>
-                    <option value="36">36</option>
-                    <option value="48">48</option>
-                    </select>
-                </p>
-                <button onClick={this.getRate}>Get my rate</button>
-                <br/>
-                {loading}
-                <br/>
-                {rateSpan}
-                
-            </div>
+
+            <Card>
+                <Card.Body>
+                    <Card.Title className="text-center">Get your quote now</Card.Title>
+                    <Card.Text className="text-center text-muted">Tell us how much you would like to borrow, and for how long to get our best available rate.</Card.Text>
+                    <Form.Range className="md-5" min="500" max="1999" step="1" value={this.state.amount} onChange={(e) => this.handleChange('amount', e)}/>
+                    <p className="text-center">£{this.state.amount}</p>
+                    <Form.Range min="12" max="48" step="12" value={this.state.months} onChange={(e) => this.handleChange('months', e)}/>
+                    <p className="text-center">over {this.state.months} months</p>
+                    <div className="d-grid gap-2">
+                        <Button variant="primary" onClick={this.getRate}>Get my quote</Button>
+                    </div>
+                    <p className="text-center">{loading}</p>
+                    {rate}
+                </Card.Body>
+            </Card>
+
+
+            
         );
     }
 }
